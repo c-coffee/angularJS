1310c3d0257b3e220d69ff0a8fce2eb6cc9b61f4
c-coffee <4165426@qq.com>
2017-02-22 22:44:37 +0800

    222

--- review/1.js
+++ review/1.js
@@ -6,27 +6,32 @@ var app = express();
 
 app.use(express.static("./public"));
 app.get("/", function (req, res) {
-    sleep(3000);
-    res.send([
-        {
-            'mno': Math.ceil(Math.random() * 1000),
-            'headpic': 'img/p0281.jpg',
-            'title': '美食1',
-            'content': '美食介绍美食介绍美食介绍美食介绍美食介绍'
-        },
-        {
-            'mno': Math.ceil(Math.random() * 1000),
-            'headpic': 'img/p4788.jpg',
-            'title': '美食2',
-            'content': '美食介绍美食介绍美食介绍美食介绍美食介绍'
-        },
-        {
-            'mno': Math.ceil(Math.random() * 1000),
-            'headpic': 'img/p2679.jpg',
-            'title': '美食3',
-            'content': '美食介绍美食介绍美食介绍美食介绍美食介绍'
-        }
-    ]);
+    setTimeout(function(){
+        res.send([
+            {
+                'mno': Math.ceil(Math.random() * 1000),
+                'headpic': 'img/p0281.jpg',
+                'price':200.50,
+                'title': 'shrimp',
+                'content': '美食介绍美食介绍美食介绍美食介绍美食介绍'
+            },
+            {
+                'mno': Math.ceil(Math.random() * 1000),
+                'headpic': 'img/p4788.jpg',
+                'price':186.00,
+                'title': 'noodles',
+                'content': '美食介绍美食介绍美食介绍美食介绍美食介绍'
+            },
+            {
+                'mno': Math.ceil(Math.random() * 1000),
+                'headpic': 'img/p2679.jpg',
+                'price':110.00,
+                'title': 'cake',
+                'content': '美食介绍美食介绍美食介绍美食介绍美食介绍'
+            }
+        ]);
+    },3000);
+
 });
 
 app.listen(3000);
